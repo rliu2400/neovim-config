dccc4463f06234c205f4db24b3323bb1858b99fb
Ruyi Lan <rliu2400@gmail.com>
2025-01-06 13:17:17 -0600

    LuaSnip Setup

--- init.lua
+++ init.lua
@@ -42,6 +42,16 @@ require("lazy").setup({
                 version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
                 -- install jsregexp (optional!).
                 build = "make install_jsregexp",
+                config = function()
+                    require("luasnip").config.set_config({ -- Setting LuaSnip config
+
+                        -- Enable autotriggered snippets
+                        enable_autosnippets = true,
+
+                        -- Use Tab (or some other key if you prefer) to trigger visual selection
+                        store_selection_keys = "<Tab>",
+                    })
+                end,
             },
         },
         config = function()
