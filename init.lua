01a215d875eae467708efcc49c1ae0a5f35595db
Ruyi Lan <rliu2400@gmail.com>
2025-01-08 22:52:46 -0600

    YAY SNIPPETS

--- init.lua
+++ init.lua
@@ -258,6 +258,21 @@ vim.g.maplocalleader = " " -- Also set local leader to Space
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true }) -- Open file explorer
 vim.keymap.set("n", "<leader>f", ":FormatWrite<CR>", { noremap = true, silent = true }) -- Format file
 
+-- Yes, we're just executing a bunch of Vimscript, but this is the officially
+-- endorsed method; see https://github.com/L3MON4D3/LuaSnip#keymaps
+vim.cmd[[
+" Use Tab to expand and jump through snippets
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+" Use Shift-Tab to jump backwards through snippets
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]]
+
+-- Load snippets from ~/.config/nvim/LuaSnip/
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/LuaSnip/" })
+
 -- Required dependencies:
 -- Install Mason-managed LSPs and formatters: clangd, pyright, jdtls, ts_ls, html, cssls, jsonls, texlab
 -- Install external formatters: stylua, black, clang-format, prettier
