e25923515c68ff0a7e325891b461a895f2b1b49e
Ruyi Lan <rliu2400@gmail.com>
2025-05-02 12:38:21 -0500

    lsp

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,6 +4,11 @@ vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, {})
 vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, {})
 vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, {})
 vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, {})
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gt", vim.lsp.buf.type_definition)
+
+vim.keymap.set("i", "<C-c>", "<Esc>", { noremap = true })
+vim.keymap.set("v", "<C-c>", "<Esc>", { noremap = true })
 
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
-
--- lua/lsp/config.lua
+++ lua/lsp/config.lua
@@ -1,5 +1,5 @@
 local lspconfig = require("lspconfig")
-local servers = { "clangd", "pyright", "jdtls", "ts_ls", "html", "cssls", "jsonls", "texlab" }
+local servers = { "clangd", "pyright", "jdtls", "ts_ls", "html", "cssls", "jsonls", "texlab", "rust_analyzer" }
 for _, server in ipairs(servers) do
     lspconfig[server].setup({})
 end
